c4c2c786b582c00daaa456e7407140c02d5277ce
BrendaSalas <l24200100@gmail.com>
2024-10-29 12:34:41 -0600

    se cambio para que mostraran los botones

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import tkinter as tk
 from ControladorElemento import ControladorElemento
 from VistaElemento import VistaElemento
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     url = "https://6720637be7a5792f05315535.mockapi.io/ElementosQuimicos"
     controlador = ControladorElemento(url)
 
     root = tk.Tk()  # Crear la ventana principal
     root.resizable(False, False)  # La ventana no es redimensionable
     app = VistaElemento(root, controlador)  # Crear la vista
-    root.mainloop()  # Ejecutar el bucle principal de la aplicación
+    root.mainloop()  # Ejecutar el bucle principal de la aplicación'''
+
+# URL de la API
+url = "https://6720637be7a5792f05315535.mockapi.io/ElementosQuimicos"
+
+# Configuración principal de la aplicación
+root = tk.Tk()
+controlador = ControladorElemento(url)
+app = VistaElemento(root, controlador)
+root.mainloop()
