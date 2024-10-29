8ac3703ba8eb8a70fca39440c25d3c738423df52
BrendaSalas <l24200100@gmail.com>
2024-10-29 12:33:56 -0600

    se cambio el controlador para poder ponerl el boton de busqueda

--- ControladorElemento.py
+++ ControladorElemento.py
@@ -5,7 +5,7 @@ class ControladorElemento:
     def __init__(self, url):
         self.url = url
 
-    def obtener_elementos(self):
+    '''def obtener_elementos(self):
         try:
             response = requests.get(self.url)
             response.raise_for_status()  # Verifica si hubo un error en la solicitud
@@ -18,4 +18,19 @@ class ControladorElemento:
             return elementos
         except requests.exceptions.RequestException as e:
             print(f"Error al obtener datos: {e}")  # O manejar el error de otra manera
+            return []'''
+    def obtener_elementos(self):
+        try:
+            # Limitar la solicitud a 100 elementos
+            response = requests.get(f"{self.url}?limit=100")
+            response.raise_for_status()
+            data = response.json()
+            # Convertir los datos en una lista de objetos Elemento
+            elementos = [
+                Elemento(item["id"], item["element"], item.get("group", ""), item.get("periodo", ""))
+                for item in data
+            ]
+            return elementos
+        except requests.exceptions.RequestException as e:
+            print(f"Error al obtener datos: {e}")
             return []
